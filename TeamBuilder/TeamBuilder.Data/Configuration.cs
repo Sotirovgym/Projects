7845759be87b1a80786182907297e4dc1e963d37
Georgi <sotirovgym@abv.bg>
2018-01-11 15:21:54 +0200

    Rename folder

---
+++ TeamBuilder/TeamBuilder.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamBuilder.Data
+{
+    public class Configuration
+    {
+        public const string ConnectionString = @"Server=SOTIROVGYM-PC\SQLEXPRESS;Database=TeamBuilder;Integrated Security=True;";
+    }
+}
